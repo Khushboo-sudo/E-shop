5f678c1453310ed334147a749c5aef14f602a77b
Khushboo-sudo <khushboosoni6746@gmail.com>
2024-09-11 22:16:31 +0530

    Added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Cart from './component/Cart'
+import Navbar from './component/Navbar'
+import Products from './component/Products'
+import ProductDetails from './component/ProductDetails'
+import Searchitems from './component/Searchitems'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route Path='/' element={< Products/>}/>
+        <Route Path='/product/:id' element={< ProductDetails/>}/>
+        <Route Path='/search/:term' element={<Searchitems />}/>   
+        <Route Path='/Cart' element={< Cart/>}/>
+           </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
---
+++ src/component/Searchitems.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Searchitems() {
+  return (
+    <div>Searchitems</div>
+  )
+}
+
+export default Searchitems
