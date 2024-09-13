8c51ac9988719ca4def5a17b7e579ac1526da294
Khushboo-sudo <khushboosoni6746@gmail.com>
2024-09-14 00:06:47 +0530

    Search-box funtion added

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,41 +1,57 @@
-import React from 'react';
-import './Navbar.css'
-import { Link } from 'react-router-dom';
-import { items } from './ItemsData';
+import React, { useState } from "react";
+import "./Navbar.css";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { items } from "./ItemsData";
 
+function Navbar({ setdata }) {
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
 
-function Navbar({setdata}) {
-  const filterByCategory= (category)=>{
-    const device=items.filter((product)=>product.category===category)
-   setdata(device)
-  }
+  const filterByCategory = (category) => {
+    const device = items.filter((product) => product.category === category);
+    setdata(device);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate(`/search/${searchTerm}`);
+    setSearchTerm("");
+  };
   return (
     <>
-       <header className='sticky-top'>
-        <div className='nav-bar'> 
-        <Link to="/" className='brand-name'>E-SHOP</Link> 
-        <div className='search-bar'>
-            <input type="text"  placeholder='Search-Products' />
-             </div>
-       <Link to="/cart" className='cart-logo'>Cart </Link>
+      <header className="sticky-top">
+        <div className="nav-bar">
+          <Link to="/" className="brand-name">
+            E-SHOP
+          </Link>
+
+          <form onSubmit={handleSubmit} className="search-bar">
+            <input
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              type="text"
+              placeholder="Search-Products"
+            />
+          </form>
+
+          <Link to="/cart" className="cart-logo">
+            Cart{" "}
+          </Link>
         </div>
-        <div className='nav-bar-wrapper'>
-            <div className='items'>Filter</div>
-            <div
-            onClick={()=>filterByCategory('mobiles')}
-             className='items'>Mobile</div>
-            <div
-            onClick={()=>filterByCategory('laptops')}
-            className='items'>laptop</div>
-            <div
-            onClick={()=>filterByCategory('tablets')}
-            className='items'>Tablet</div>
-           
-            
-            </div>
-       </header>
-       </>
-  )
+        <div className="nav-bar-wrapper">
+          <div className="items">Filter</div>
+          <div onClick={() => filterByCategory("mobiles")} className="items">
+            Mobile
+          </div>
+          <div onClick={() => filterByCategory("laptops")} className="items">
+            laptop
+          </div>
+          <div onClick={() => filterByCategory("tablets")} className="items">
+            Tablet
+          </div>
+        </div>
+      </header>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/component/Searchitems.jsx
+++ src/component/Searchitems.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Products from "./Products";
+import { items } from "./ItemsData";
+
 
 function Searchitems() {
-  return (
-    <div>Searchitems</div>
+  
+  const { term } = useParams();
+  const [searchItem, setSearchItem] = useState([]);
+  useEffect(() => {
+    const filteredProducts = () => {
+      const data = items.filter((p) =>
+        p.title.toLowerCase().includes(term.toLowerCase()))
+      
+      
+      
+      setSearchItem(data)
+     
+    };
+    filteredProducts()
+    
+  }, [term]);
+  return(
+  <Products items={searchItem}/>
   )
+   
 }
 
-export default Searchitems
+export default Searchitems;
